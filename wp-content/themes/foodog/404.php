6c5c55303dfbaa0680337f99c3626744e877ff65
W-mseg <marco.segretario2705@gmail.com>
2020-05-20 12:16:50 +0200

    add 404 error

---
+++ wp-content/themes/foodog/404.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404</title>
+</head>
+<body>
+<style>
+    .error-404{
+        display: flex;
+        justify-content: center;
+    }
+    .p_404{
+        display: flex;
+        justify-content: center;
+    }
+</style>
+<h1 class="error-404">Error 404 not found</h1>
+<p class="p_404">If you see this page, go back, your URL don't exist</p>
+</body>
+</html>
