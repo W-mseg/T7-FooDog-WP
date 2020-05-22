7440159fbb8ddd8d1ce71c946f5e8792c2fffff2
stephaneleonard <stefleonard97@gmail.com>
2020-05-22 10:48:34 +0200

    deleted useless method

--- wp-content/themes/foodog/functions.php
+++ wp-content/themes/foodog/functions.php
@@ -74,15 +74,6 @@ function foodog_category_class()
     return $output;
 }
 
-function foodog_remove_share()
-{
-    remove_filter('the_content', 'sharing_display', 19);
-    remove_filter('the_excerpt', 'sharing_display', 19);
-    if (class_exists('Jetpack_Likes')) {
-        remove_filter('the_content', array(Jetpack_Likes::init(), 'post_likes'), 30, 1);
-    }
-}
-
 
 
 add_theme_support('post-thumbnails');
