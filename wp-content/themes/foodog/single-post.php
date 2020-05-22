53f20b2a03c522e8195be469a61ceb022fc385ea
stephaneleonard <stefleonard97@gmail.com>
2020-05-22 12:24:25 +0200

    added avatar images and begining of the pagination on single page

--- wp-content/themes/foodog/single-post.php
+++ wp-content/themes/foodog/single-post.php
@@ -26,13 +26,37 @@ get_header();
                     <h2 class="single-title mb-4"><?= the_title() ?></h2>
                     <?php the_post_thumbnail('full', ['class' => 'img-fluid mb-4', 'style' => 'height:auto']) ?>
                     <div class="d-flex bd-highlight border-top border-bottom mb-4">
-                        <div class="p-2 flex-grow-1 bd-highlight my-auto"><?php get_avatar(get_the_author_meta('ID'), 32);
-                                                                            the_author(); ?></div>
+                        <div class="p-2 flex-grow-1 bd-highlight my-auto"><?= get_avatar(get_the_author_meta('ID'), 32); ?>by:<?php get_avatar(get_the_author_meta('ID'), 32);
+                                                                                                                                the_author(); ?></div>
                         <div class="p-2 bd-highlight my-auto">COMMENTS</div>
                         <div class="p-2 bd-highlight my-auto">SHARE</div>
                         <div class="p-2 bd-highlight my-auto">SOCIALS</div>
                     </div>
-                    <?= the_content() ?>
+                    <div class="single-content">
+                        <?= the_content() ?>
+                    </div>
+                    <div class="sinlge-article-nav border-top">
+                        <div class="row">
+                            <div class="col-6 border-right">
+                                <?php $prev_post = get_previous_post(); ?>
+                                <?= $prev_post->post_title ?>
+                            </div>
+                            <div class="col-6">
+                                <?php $next_post = get_next_post(); 
+                                ?>
+                                <?= $next_post->post_title ?>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div class="sinlge-article-author border-top d-flex">
+                        <?= get_avatar(get_the_author_meta('ID'), 96); ?>
+                        <p><?php the_author_meta('description'); ?></p>
+                    </div>
+                    <div class="sinlge-article-related border-top">
+                        <h3>you might also like</h3>
+                    </div>
                     <div class="single-comments border-top">
                         <?php if (comments_open() || get_comments_number()) {
                             comments_template();
