023e66cd4c3f55c5a6a4731c09d61035b26728f6
W-mseg <marco.segretario2705@gmail.com>
2020-05-22 12:20:11 +0200

    update loops

--- wp-content/themes/foodog/404.php
+++ wp-content/themes/foodog/404.php
@@ -9,6 +9,7 @@
         justify-content: center;
     }
 </style>
+
 <h1 class="error-404">Error 404 not found</h1>
 <p class="p_404">If you see this page, go back, your URL don't exist</p>
 <?php get_footer(); ?>
