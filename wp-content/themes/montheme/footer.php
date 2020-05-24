6e735004059d52cf9b293def34cc16ded77809e7
LucianoCaturano <luciano.caturano@gmail.com>
2020-05-24 15:11:30 +0200

    creation menu header/menu footer

--- wp-content/themes/montheme/footer.php
+++ wp-content/themes/montheme/footer.php
@@ -1,3 +1,32 @@
+</div>
+    <footer>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul>
+                <li>
+                    <h2>CATEGORIES</h2>
+                    <p>
+                        <?php wp_nav_menu([
+                            'theme_location'=>'footer',
+                            'container'=>false,
+                            'menu_class'=> 'navbar-nav mr-auto',
+                        ])
+                        ?>
+                    </p>
+                </li>
+                <li>
+                    <h2>POPULAR POSTS</h2>
+                </li>
+                <li>
+                    <h2>INSTAGRAM</h2>
+                </li>
+            </ul>
+        </div>
+    </footer>
     <?php wp_footer() ?>
 </body>
 </html>
--- wp-content/themes/montheme/header.php
+++ wp-content/themes/montheme/header.php
@@ -5,7 +5,52 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>FooDog</title>
     <?php wp_head() ?>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <?php wp_nav_menu([
+                'theme_location'=>'header',
+                'container'=>false,
+                'menu_class'=> 'navbar-nav mr-auto',
+
+        ])
+        ?>
+
+        <!--
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">NUTRITION <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">WELLNESS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">LIFESTYLE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">COMMUNITY</a>
+            </li>
+        </ul>
+
+
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        -->
+    </div>
+</nav>
+
+    <div class="container">
+
+
+
