b938c465ed4921945321847a9f68941c6c562850
LucianoCaturano <luciano.caturano@gmail.com>
2020-05-27 20:49:42 +0200

    add instagram widget

--- wp-content/themes/foodog/footer.php
+++ wp-content/themes/foodog/footer.php
@@ -1,78 +1,75 @@
-</div>
-<!-- Footer -->
-<footer class="page-footer font-small blue pt-4">
+    </div>
+    <!-- Footer -->
+    <footer class="page-footer">
 
-    <!-- Footer Links -->
-    <div class="container-fluid text-center text-md-left">
+        <!-- Footer Links -->
+        <div class="container-fluid text-center text-md-left">
 
-        <!-- Grid row -->
-        <div class="row">
+            <!-- Grid row -->
+            <div class="row">
 
-            <!-- Grid column -->
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3">
 
-            <hr class="clearfix w-100 d-md-none pb-3">
+                    <!-- Links navbar -->
+                    <h5 class="text-uppercase">categories</h5>
 
-            <!-- Grid column -->
-            <div class="col-md-3 mb-md-0 mb-3">
-
-                <!-- Links -->
-                <h5 class="text-uppercase">categories</h5>
-
-                <?php wp_nav_menu([
-                    'theme_location'=>'footer',
-                    'container'=>false,
-                    'menu_class'=> 'navbar-nav mr-auto',
-                ])
-                ?>
-
-            </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div class="col-md-3 mb-md-0 mb-3">
-
-                <!-- Links -->
-                <h5 class="text-uppercase">popular posts</h5>
-
-                <div class="popular">
-                    <?php
-                    require_once('loop/loop_popular.php');
+                    <?php wp_nav_menu
+                    ([
+                        'theme_location'=>'footer',
+                        'container'=>false,
+                        'menu_class'=> 'navbar-nav mr-auto',
+                    ])
                     ?>
+
                 </div>
-            </div>
-            <!-- Grid column -->
-            <!-- Grid column -->
-            <div class="col-md-6 mt-md-0 mt-3">
 
-                <!-- Content -->
-                <h5 class="text-uppercase">instagram</h5>
-                <p>API insta (or instagram feed plugin?)</p>
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3">
+
+                    <!-- Links popular posts -->
+                    <h5 class="text-uppercase">popular posts</h5>
+
+                    <div class="popular">
+                        <?php
+                        require_once('loop/loop_popular.php');
+                        ?>
+                    </div>
+                </div>
+
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3">
+
+                    <!-- Content Instagram -->
+                    <h5 class="text-uppercase">instagram</h5>
+
+                    <!-- InstagramWidget -->
+                    <script src="https://snapwidget.com/js/snapwidget.js"></script>
+                    <iframe src="https://snapwidget.com/embed/831830" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:50%; "></iframe>
+                </div>
 
             </div>
+
 
         </div>
-        <!-- Grid row -->
+        <!-- Footer Links -->
 
-    </div>
-    <!-- Footer Links -->
+        <!-- Copyright -->
+        <div class="footer-social text-right">
 
-    <!-- Copyright -->
-    <div class="footer-social text-right">
+            <!--Facebook-->
+            <a class="btn-floating btn-lg btn-fb" type="button" role="button"><i class="fab fa-facebook-f"></i></a>
+            <!--Twitter-->
+            <a class="btn-floating btn-lg btn-tw" type="button" role="button"><i class="fab fa-twitter"></i></a>
+            <!--Instagram-->
+            <a class="btn-floating btn-lg btn-ins" type="button" role="button"><i class="fab fa-instagram"></i></a>
 
-        <!--Facebook-->
-        <a class="btn-floating btn-lg btn-fb" type="button" role="button"><i class="fab fa-facebook-f"></i></a>
-        <!--Twitter-->
-        <a class="btn-floating btn-lg btn-tw" type="button" role="button"><i class="fab fa-twitter"></i></a>
-        <!--Instagram-->
-        <a class="btn-floating btn-lg btn-ins" type="button" role="button"><i class="fab fa-instagram"></i></a>
+        </div>
 
-    </div>
-    <!-- Copyright -->
+    </footer>
 
-</footer>
-<!-- Footer -->
 
-<?php wp_footer() ?>
+    <?php wp_footer() ?>
 </body>
 </html>
 
--- wp-content/themes/foodog/header.php
+++ wp-content/themes/foodog/header.php
@@ -10,40 +10,40 @@
 </head>
 <body>
 
-<?echo get_search_form()?>
-
-<!--Header Social & search icons-->
-<div class="text-right">
-    <!--Facebook-->
-    <a class="fb-ic mr-3" role="button"><i class="fab fa-lg fa-facebook-f"></i></a>
-    <!--Twitter-->
-    <a class="tw-ic mr-3" role="button"><i class="fab fa-lg fa-twitter"></i></a>
-    <!--Instagram-->
-    <a class="ins-ic mr-3" role="button"><i class="fab fa-lg fa-instagram"></i></a>
-    <!--search-->
-    <a><i class="fas fa-search" aria-hidden="false"></i></a>
-</div>
-
-<div class="text-center">
-    <img src="http://localhost:8000/wp-content/uploads/2020/05/logo.jpeg" class="img-fluid"  >
-</div>
-
-
-<nav class="navbar navbar-expand-lg navbar-light ">
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <?php wp_nav_menu([
-            'theme_location'=>'header',
-            'container'=>false,
-            'menu_class'=> 'navbar-nav mr-auto',
-
-        ])
-        ?>
+    <!--Header Social & search icons-->
+    <div class="text-right">
+        <!--Facebook-->
+        <a class="fb-ic mr-3" role="button"><i class="fab fa-lg fa-facebook-f"></i></a>
+        <!--Twitter-->
+        <a class="tw-ic mr-3" role="button"><i class="fab fa-lg fa-twitter"></i></a>
+        <!--Instagram-->
+        <a class="ins-ic mr-3" role="button"><i class="fab fa-lg fa-instagram"></i></a>
+        <!--search-->
+        <a><i class="fas fa-search" aria-hidden="false"></i></a>
     </div>
-</nav>
 
+    <!--Logo Foodog-->
+    <div class="text-center">
+        <img src="http://localhost:8000/wp-content/uploads/2020/05/logo.jpeg" class="img-fluid"  >
+    </div>
 
-<div class="container">
+    <!--Menu Foodog-->
+    <nav class="navbar navbar-expand-lg navbar-light ">
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <?php wp_nav_menu
+                ([
+                    'theme_location'=>'header',
+                    'container'=>false,
+                    'menu_class'=> 'navbar-nav mr-auto',
+
+                ])
+            ?>
+        </div>
+    </nav>
+
+    <!--Beginning posts-->
+    <div class="container">
 
 
 
