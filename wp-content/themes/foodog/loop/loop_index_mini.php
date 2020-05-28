a6d118f3da03b7b05f570e1f254e7a19b2721213
stephaneleonard <stefleonard97@gmail.com>
2020-05-28 15:40:57 +0200

    started debug and style front page post

--- wp-content/themes/foodog/loop/loop_index_mini.php
+++ wp-content/themes/foodog/loop/loop_index_mini.php
@@ -1,32 +1,27 @@
 <?php
 
-if (have_posts()):
+if (have_posts()) :
     $recent_post = wp_get_recent_posts(array(
-        'numberposts'=>4,
-        'ignore_sticky_posts'=>1,
-        'post_status'=>'publish',
+        'numberposts' => 4,
+        'ignore_sticky_posts' => 1,
+        'post_status' => 'publish',
     ));
 
-    foreach ($recent_post as $post): ?>
+    foreach ($recent_post as $post) : ?>
 
-        <div class="card card_mini">
-            <a href="<?= get_permalink($post['ID']) ?>">
-            <img
-                    class="card-img-top"
-                    src="<?= get_the_post_thumbnail($post['ID']); ?>
-            </a>
-                <div class="card-body article">
-
-            <h5 class="card-title" style="text-align: center"><?= $post['post_title'] ?></h5>
-            <p class="card-text"><?= $post['post_excerpt'] ?></p>
-            <a href="<?= get_permalink($post['ID']) ?>" class="btn btn-primary">lire plus</a>
-        </div>
-        </div>
+        <a href="<?= get_permalink($post['ID']) ?>" class="custom-card col-md-6">
+            <div class="card">
+                <?= get_the_post_thumbnail($post['ID'], 'thumnail', ['class' => 'img-fluid mb-4', 'style' => 'height:auto']); ?>
+                <div class="card-body">
+                    <h5 class="card-title">Wrapped with a tag</h5>
+                </div>
+            </div>
+        </a>
 
 
-    <?php endforeach;wp_reset_query(); ?>
+    <?php endforeach;
+    wp_reset_query(); ?>
 
-<?php else: ?>
+<?php else : ?>
     <h1>Pas d'articles</h1>
-<?php endif;?>
-
+<?php endif; ?>
