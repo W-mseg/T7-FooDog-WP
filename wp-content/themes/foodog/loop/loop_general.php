580944a0d0f29526bd97ca3c47689bcee76b4434
W-mseg <marco.segretario2705@gmail.com>
2020-05-29 10:32:58 +0200

    update

--- wp-content/themes/foodog/loop/loop_general.php
+++ wp-content/themes/foodog/loop/loop_general.php
@@ -9,7 +9,7 @@ while
     <div class="card featured_card" style="width: 500px;">
 
         <a href="<?= get_permalink() ?>">
-            <img class="card-img-top" src="<?= get_the_post_thumbnail(); ?>">
+            <img class="card-img-top" src="<?= get_the_post_thumbnail()?>">
         </a>
 
 
@@ -18,7 +18,7 @@ while
             <h5 class=" card-title" style="text-align: center"><?= the_title() ?></h5>
             <p class="card-text"><?= the_excerpt() ?></p>
             <a href="<?= the_permalink() ?>" class="btn btn-primary">lire plus</a>
-
+            </div>
         </div>
     </div>
 
--- wp-content/themes/foodog/loop/loop_index.php
+++ wp-content/themes/foodog/loop/loop_index.php
@@ -8,13 +8,20 @@ if (have_posts()) :
         'post_status' => 'publish',
     ));
 
+
     foreach ($recent_post as $post) : ?>
+<?php
+$test = get_the_category();
+?>
+
 
         <a href="<?= get_permalink($post['ID']) ?>" class="custom-card">
             <div class="card">
                 <?= get_the_post_thumbnail($post['ID'], 'thumnail', ['class' => 'img-fluid', 'style' => 'height:auto']); ?>
                 <div class="card-body">
-                    <?php the_category(' ', $post['ID']); ?>
+                    <?php foreach ($test as $category){
+                            echo '<p>'.$category->name.'</p>';
+                    }?>
                     <h5 class="card-title"><?= $post['post_title'] ?></h5>
                 </div>
             </div>
